3192e18c9a9976199692db14fbf01983c3c2166b
Michele <michelework02@gmail.com>
2026-04-14 13:00:36 +0200

    esercitazione javascript con funzioni

---
+++ L14 - Javascript funzioni/script.js
@@ -0,0 +1,84 @@
+/**
+ * Javascript: funzioni
+ * 
+ * La funzione è un blocco di codice riutilizzabile. 
+ * 
+ * Ogni funzione al suo interno ha una o più istruzioni per
+ * svolgere un determinato compito.
+ */
+
+// Definisco la funzione "somma"
+function somma(variabile1, variabile2) {
+    console.log(
+        variabile1 + variabile2
+    );
+}
+
+// Chiamo la funzione "somma"
+somma(100, 100);
+
+/**
+ * FUNZIONI DI RITORNO
+ */
+function sottrazione(a, b) {
+    return a - b;
+}
+
+const risultato = sottrazione(50, 10);
+
+console.log( risultato );
+
+/**
+ * ESERCIZIO SVOLTO - 1
+ * 
+ * Crea una funzione che richiede nome e cognome a un
+ * utente e poi stampa nella console la presentazione.
+ * 
+ * - [x] Calcolo età
+ * - Dire se l'utente è maggiorenne o meno
+ */
+function presentaUtente() {
+    const nome = prompt("Dimmi il tuo nome");
+    const cognome = prompt("Dimmi il tuo cognome");
+
+    const annoDiNascita = prompt("Dimmi la tua data di nascita");
+    const eta = (new Date()).getFullYear() - annoDiNascita;
+
+    if (eta >= 18) {
+        console.log(
+            "Ciao " + nome + " " + cognome + 
+            ", ho calcolato che hai " + eta + " anni." +
+            "Per cui sei maggiorenne"
+        );
+    } else {
+        console.log(
+            "Ciao " + nome + " " + cognome + 
+            ", ho calcolato che hai " + eta + " anni." +
+            "Per cui sei MINORENNE!!!"
+        );
+    }
+}
+
+// presentaUtente();
+
+/**
+ * ESERCIZIO SVOLTO 2
+ * 
+ * Calcolare i gradi farenheit avendo quelli celsius in input
+ */
+function daCelsiusAFarenheit(gradiCelsius) {
+    return (gradiCelsius * 9/5) + 32
+}
+
+function daFarenheitACelsius(gradiFarenheit) {
+    return (gradiFarenheit - 32) * 5/9
+}
+
+console.log( daFarenheitACelsius(68) )
+
+/**
+ * ESERCIZIO SVOLTO 3
+ * 
+ * - Da celsius a kelvin
+ * - da kelvin a celsius
+ */
